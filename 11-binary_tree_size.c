0556cb90b9c77c3e303b583b9c7a269a22eb1fd1
ejimvincent <ejimovc@gmail.com>
2024-03-26 20:30:58 +0100

    Merginging

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+
+
+/**
+ * binary_tree_size - Goes through a binary tree using post-order traversal
+ *
+ * @tree: A pointer to the root node of the tree to traverse
+ * @func: A pointer to a function to call for each node
+ *        The value in the node must be passed as a parameter to this function
+ *
+ * Description: If tree or func is NULL, do nothing
+ *
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+
+}
