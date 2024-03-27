40c6af49e44069de119dae9533ffa33276e892aa
ejimvincent <ejimovc@gmail.com>
2024-03-27 04:21:21 +0100

    upload

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_leaves - Counts the leaves in a binary tree.
+ *
+ * @tree: A pointer to the node of the tree to count leaves.
+ *
+ * Return: The number of leaves in the tree, or 0 if tree is NULL.
+ *
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+    size_t result;
+    if (tree == NULL)
+        return (0);
+
+    if (tree->left == NULL && tree->right == NULL)
+        return (1);
+
+    result = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+
+    return (result);
+}
---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree
+ *
+ * @tree: A pointer to the root node of the tree to measure.
+ *
+ * Return: The balance factor of the tree, or 0 if tree is NULL.
+ *
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+    size_t left_height = 0;
+    size_t right_height = 0;
+
+    if (tree == NULL)
+        return (0);
+
+    if (tree->left)
+        left_height = 1 + binary_tree_height(tree->left);
+    if (tree->right)
+        right_height = 1 + binary_tree_height(tree->right);
+}
