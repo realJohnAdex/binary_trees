16ff5b04feebb89e9e79288e01bc72e62bc036c9
ejimvincent <ejimovc@gmail.com>
2024-03-27 11:22:14 +0100

    Triming white spaces

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -17,6 +17,5 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	binary_tree_inorder(tree->left, func);
 	func(tree->n);
 	binary_tree_inorder(tree->right, func);
-	tree->
 
 }
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -3,7 +3,8 @@
 
 
 /**
- * binary_tree_postorder - Goes through a binary tree using post-order traversal
+ * binary_tree_postorder - Goes through a binary
+ * 						tree using post-order traversal
  *
  * @tree: A pointer to the root node of the tree to traverse
  * @func: A pointer to a function to call for each node
