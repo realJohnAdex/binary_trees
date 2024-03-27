fc047bd2172501f51f1e710ef512515ffbe7aed8
ejimvincent <ejimovc@gmail.com>
2024-03-27 11:35:59 +0100

    upload

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,22 +9,21 @@
  * Return: A pointer to the sibling node, or NULL if no sibling exists
  */
 
-
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (node == NULL || node->parent == NULL)
-        return (NULL);
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
 
-    if (node->parent->left == node)
-    {
-        if (node->parent->right != NULL)
-            return (node->parent->right);
-    }
-    else
-    {
-        if (node->parent->left != NULL)
-            return (node->parent->left);
-    }
+	if (node->parent->left == node)
+	{
+		if (node->parent->right != NULL)
+			return (node->parent->right);
+	}
+	else
+	{
+		if (node->parent->left != NULL)
+			return (node->parent->left);
+	}
 
-    return (NULL);
+	return (NULL);
 }
